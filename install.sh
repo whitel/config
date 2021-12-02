caf376f96b97243271cb6985e28f78e52cd78f72
whitel <heriskent@gmail.com>
2021-12-02 14:59:03 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,8 @@ cd ~/config
 if [[ $option == "" ]]; then
 	echo "[v] Installing .vimrc .bashrc .gitconfig..."
 	cp vimrc ~/.vimrc
-	# cp bashrc ~/.bashrc
+	cp bashrc ~/.bashrc
 	cp gitconfig ~/.gitconfig
-elif [[ $option == "vim" ]]; then
-	echo "[v] Install vim..."
-	if [[ -d ~/.vim ]]; then
-		mv ~/.vim ~/.vim.backup
-	fi
-	cp -r vim ~/.vim
-	echo "[x] the funcion is not implemented, please manually install vim plugins."
-elif [[ $option == "pass" ]]; then
-	echo "[v] Install password..."
-	cp Passwords.kdbx $chome
 else
 	echo "[x] unknown command" \"$option\"
 	echo ""
