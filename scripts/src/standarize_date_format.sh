4b74bed9325a47a8323909da4b9747d5ff3ab78c
whitel <heriskent@gmail.com>
2022-02-02 01:39:19 +0800

    Update

--- scripts/src/standarize_date_format.sh
+++ scripts/src/standarize_date_format.sh
@@ -5,30 +5,32 @@ usage () {
     echo ""
     echo "Usage:"
     echo "        sf        Print this page"
-    echo "        sf do     Format filename to Date Format"
+    echo "        sf dof    Format filename to Date Format"
+    echo "        sf dod    Format folders name to Data Format"
     echo "        sf undo   Unformat all filename from Date Format"
 }
 
 solve () {
+    TYPE=$1 
 	# Step 1. replace all spaces to '-'
 	rename 's/ /-/g' *
 	
 	# Step 2.1 format 20200101 to 2020-01-01
-	for f in $(find . -maxdepth 1 -type f -printf '%P\n' | grep -E "[0-9]{8}")
+	for f in $(find . -maxdepth 1 -type ${TYPE} -printf '%P\n' | grep -E "[0-9]{8}")
 	do
 		FILENAME=$(echo ${f} | sed -e 's/.\{6\}/&-/' -e 's/.\{4\}/&-/')
 		mv -n "${f}" "${FILENAME}" 
 	done
 	
 	# Step 2.2 format 2020-01-01-hello.md to 2020-01-01_hello.md
-	for f in $(find . -maxdepth 1 -type f -printf '%P\n' | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}-")
+	for f in $(find . -maxdepth 1 -type ${TYPE} -printf '%P\n' | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}-")
 	do
 		FILENAME=$(echo ${f} | sed -e 's/./_/11')
 		mv -n "${f}" "${FILENAME}"
 	done
 	
 	# Step 3. add 2020-01-01_ to all files
-	for f in $(find . -maxdepth 1 -type f -printf '%P\n' | grep -v -E "[0-9]{4}-[0-9]{2}-[0-9]{2}")
+	for f in $(find . -maxdepth 1 -type ${TYPE} -printf '%P\n' | grep -v -E "[0-9]{4}-[0-9]{2}-[0-9]{2}")
 	do
 		FILENAME=$(date -r "$f" +'%Y-%m-%d'_${f})
 		mv -n "${f}" "${FILENAME}"
@@ -36,7 +38,8 @@ solve () {
 }
 
 unsolve () {
-    for f in $(find . -maxdepth 1 -type f -printf '%P\n' | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}_")
+    TYPE=$1 
+    for f in $(find . -maxdepth 1 -type ${TYPE} -printf '%P\n' | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}_")
 	do
 		FILENAME=$(echo ${f} | cut -c 12-)
 		mv -n "${f}" "${FILENAME}"
@@ -45,10 +48,14 @@ unsolve () {
 
 main () {
     ARG=$1
-    if [[ "do" ==  ${ARG} ]]; then
-        solve
-    elif [[ "undo" == ${ARG} ]]; then
-        unsolve
+    if [[ "dof" ==  ${ARG} ]]; then
+        solve f
+    elif [[ "dod" == ${ARG} ]]; then
+        solve d
+    elif [[ "undof" == ${ARG} ]]; then
+        unsolve f
+    elif [[ "undod" == ${ARG} ]]; then
+        unsolve d
     else
         usage
     fi
