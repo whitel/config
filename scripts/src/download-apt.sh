b939a15b693018fd6e707b26784e38b374071099
whitel <heriskent@gmail.com>
2022-02-03 14:22:44 +0800

    Update

---
+++ scripts/src/download-apt.sh
@@ -0,0 +1,9 @@
+download_apt () {
+    PAC_NAME=$1
+    apt download ${PAC_NAME}
+    APT_NAME=$(ls | grep ${PAC_NAME} | grep ".deb$")
+
+    dpkg-deb -xv ${APT_NAME} ${PAC_NAME} && rm ${APT_NAME}
+}
+
+download_apt $@
