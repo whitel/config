08222b040dd698570f5cf9ff8a86a63ca4fdda26
whitel <heriskent@gmail.com>
2022-03-21 14:57:28 +0800

    Update

--- scripts/src/create_env.sh
+++ scripts/src/create_env.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
+backup() {
+    if [[ -d ~/.create_env.bak ]]; then
+     rm ~/.create_env.bak -rf
+    fi
+    mkdir ~/.create_env.bak
+    mv ~/playground/* ~/.create_env.bak
+}
+
 CNT=$1
+
+backup
 
 for (( c=1; c<=${CNT}; c++ ))
 do  
-    cp ~/projects/algorithm/code_template ~/playground/problem0${c} -r
+    cp -r ~/projects/algorithm/code_template ./playground/problem0${c}
 done
 
